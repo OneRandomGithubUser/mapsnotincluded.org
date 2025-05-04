5bf22921950da00785937acfe83bf9d7dba7798a
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2025-05-03 20:03:56 -0400

    Update CreateCascadingError.ts

--- frontend/src/components/CreateCascadingError.ts
+++ frontend/src/components/CreateCascadingError.ts
@@ -1,3 +1,5 @@
+// TODO: make the base error explicitly set or not set
+// TODO: cascading warning/info/log
 export function createError(locationName: string, msg: string, doConsoleLog: Boolean = false, baseError?: unknown): Error {
     const prefixedMsg = `[${locationName}] ❌ ${msg}`;
     const errorOptions = baseError ? {cause: baseError} : undefined;
